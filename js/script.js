667285aebffcb17ac7c57901e28da268a6d37b15
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-06-29 16:33:33 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,58 +1,78 @@
 let valor = prompt("Digite o valor a ser sacado?")
 let saque = Number(valor)
-document.write(`<h3 class="saque">VOCE FICOU R$${valor} MAI$ RICO!!</h3>`)
+document.write(`<h3>VOCE FICOU R$${valor} MAI$ RICO!!</h3>`)
+
+let notas = []
+let moedas = []
 
 while( saque >= 200){
-    document.write('<img src="img/200-real.jpg" class="notas">')
+    notas.push('img/200-real.jpg')
     saque = saque - 200
 }
 while( saque >= 100){
-    document.write('<img src="img/100-real.jpg" class="notas">')
+    notas.push('img/100-real.jpg')
     saque = saque - 100
 }
 while( saque >= 50){
-    document.write('<img src="img/50-real.jpg" class="notas">')
+    notas.push('img/50-real.jpg')
     saque = saque - 50
 }
 while( saque >= 20){
-    document.write('<img src="img/20-real.jpg" class="notas">')
+    notas.push('img/20-real.jpg')
     saque = saque - 20
 }
 while( saque >= 10){
-    document.write('<img src="img/10-real.jpg" class="notas">')
+    notas.push('img/10-real.jpg')
     saque = saque - 10
 }
 while( saque >= 5){
-    document.write('<img src="img/5-real.jpg" class="notas">')
+    notas.push('img/5-real.jpg')
     saque = saque - 5
 }
 while( saque >= 2){
-    document.write('<img src="img/2-real.jpg" class="notas">')
+    notas.push('img/2-real.jpg')
     saque = saque - 2
 }
 while( saque >= 1){
-    document.write('<img src="img/1-real.png" class="moedas">')
+    moedas.push('img/1-real.png')
     saque = saque - 1
 }
 while( saque >= .5){
-    document.write('<img src="img/50-cents.png" class="moedas">')
+    moedas.push('img/50-cents.png')
     saque = saque - .5
 }
 while( saque >= .25){
-    document.write('<img src="img/25-cents.png" class="moedas">')
+    moedas.push('img/25-cents.png')
     saque = saque - .25
 }
 while( saque >= .1){
-    document.write('<img src="img/10-cents.png" class="moedas">')
+    moedas.push('img/10-cents.png')
     saque = saque - .1
 }
 while( saque >= .05){
-    document.write('<img src="img/5-cents.png" class="moedas">')
+    moedas.push('img/5-cents.png')
     saque = saque - .05
 }
 while( saque > 0.009){
-    document.write('<img src="img/juquinha.png" class="juquinha">')
+    moedas.push('img/juquinha.png')
     saque = saque - .01
 }
+document.write(`<div class="saqueNotas">`)
+var rotate = 50
+for(nota of notas){
+    document.write(`<img src="${nota}" class="notas" style="transform: rotate(${rotate}deg);"></img>`)
+    var rotate = rotate + 15
+}
+document.write(`</div>`)
 
-console.log(saque)
+document.write(`<div class="saqueMoedas">`)
+var leftV = 12
+var topV = 50
+var rotatem = 0
+for(moeda of moedas){
+    document.write(`<img src="${moeda}" class="moedas" style="transform: rotate(${rotatem}deg); top: ${topV}vh; left: ${leftV}vw;"></img>`)
+    var leftV = leftV + 3
+    var topV = topV + 2
+    var rotatem = rotatem + 55
+}
+document.write(`</div>`)
